9c011922bcb50b78c8829aacc3e4e748f6a35ac5
Natalya Ivanova <natalyas.ivanova@gmail.com>
2021-10-11 14:45:25 +0300

    Ex 1 extra 1 Use Dialog from @reach/dialog

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,20 +1,36 @@
-// 🐨 you'll need to import React and ReactDOM up here
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Logo } from './components/logo'
+import { Dialog } from '@reach/dialog'
+
+import '@reach/dialog/styles.css'
 
 const App = () => {
+	const [modalState, setModalState] = React.useState('closed')
+
 	function login () {
-		alert('Login button clicked!')
+		setModalState('login')
 	}
 
 	function register () {
-		alert('Register button clicked!')
+		setModalState('register')
+	}
+
+	function close () {
+		setModalState('closed')
 	}
 
 	return (
 		<div>
 			<Logo />
+			<Dialog isOpen={modalState === 'login'} aria-labelledby="login dialog">
+				<button className="close-button" onClick={close}>close</button>
+				<p>Login button clicked!</p>
+			</Dialog>
+			<Dialog isOpen={modalState === 'register'} aria-labelledby="register dialog">
+				<button className="close-button" onClick={close}>close</button>
+				<p>Register button clicked!</p>
+			</Dialog>
 			<h1>Bookshelf</h1>
 			<button onClick={login}>Login</button>
 			<button onClick={register}>Register</button>
