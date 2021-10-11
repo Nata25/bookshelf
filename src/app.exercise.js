86479616c19bd536d306cee554e07b51398de2d7
Natalya Ivanova <natalyas.ivanova@gmail.com>
2021-10-11 23:17:39 +0300

    Ex 4 extra 1 Save user status in browser, add token to headers

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -5,6 +5,7 @@ import * as React from 'react'
 import * as auth from 'auth-provider'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
+import { client } from 'utils/api-client.exercise'
 
 function App() {
   const [user, setUser] = React.useState(null)
@@ -23,6 +24,24 @@ function App() {
     auth.logout()
     setUser(null)
   }
+
+  async function getUser () {
+    const token = await auth.getToken()
+    if (token) {
+      const headers = {
+        Authorization: `Bearer ${token}`,
+      }
+      client('me', {headers}).then(data => {
+        setUser(data.user)
+      })
+    } else {
+      setUser(null)
+    }
+  }
+
+  React.useEffect(() => {
+    getUser()
+  }, [])
 
   return (
     <div>
