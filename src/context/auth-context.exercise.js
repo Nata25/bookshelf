e30457a830228a6138cb6124656499ef4c69a839
Natalya Ivanova <natalyas.ivanova@gmail.com>
2021-10-15 10:12:14 +0300

    Ex 7 extra 1 Replace direct call of React.useContext with useAuth hook

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -1,6 +1,15 @@
 import React from 'react'
 
-const AuthContext = React.createContext({})
+const AuthContext = React.createContext()
 
-export {AuthContext}
+function useAuth () {
+	const context = React.useContext(AuthContext)
+	if (context === undefined) {
+		throw new Error('`useAuth` should be used inside AuthContext.provider.')
+	} else {
+		return context // { user, register, login, logout }
+	}
+}
+
+export {AuthContext, useAuth}
 
