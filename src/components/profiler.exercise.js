d69e1cfdc28988455de9136184abf71bdee84b22
Natalya Ivanova <natalyas.ivanova@gmail.com>
2021-10-18 16:35:53 +0300

    Ex 9 extra 3 Add Profiler to book screen

--- src/components/profiler.exercise.js
+++ src/components/profiler.exercise.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {client} from 'utils/api-client'
 
 function Profiler ({id, phases, children}) {
+	if (!phases || phases.includes('mount')) console.log(`Profiler for ${id} init.`)
+
 	const callback = (...data) => {
 		const [id, phase, actualDuration, baseDuration, startTime, commitTime] = data
 		if (!phases || phases.includes(phase)) {
