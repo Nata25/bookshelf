b207d94164a6f1c81f3eeb93ef4f5107a3741346
Natalya Ivanova <natalyas.ivanova@gmail.com>
2021-10-18 16:23:29 +0300

    Ex 9 extra 3 Send data from Profiler to mocked backed

--- src/components/profiler.exercise.js
+++ src/components/profiler.exercise.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import {client} from 'utils/api-client'
 
 function Profiler ({id, children}) {
 	const callback = (...data) => {
-		console.log(...data)
+		const [id, phase, actualDuration, baseDuration, startTime, commitTime] = data
+		client('profile', {data: {
+			id, phase, actualDuration, baseDuration, startTime, commitTime
+		}})
 	}
 	return (
 		<React.Profiler id={id} onRender={callback}>
