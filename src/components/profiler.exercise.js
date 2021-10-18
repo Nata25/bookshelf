df36f868ef0fafbae1d4e7b09b37573503097203
Natalya Ivanova <natalyas.ivanova@gmail.com>
2021-10-18 16:50:47 +0300

    Ex 9 extra 3 Group requests to /profile and send with time interval

--- src/components/profiler.exercise.js
+++ src/components/profiler.exercise.js
@@ -2,16 +2,31 @@ import React from 'react'
 import {client} from 'utils/api-client'
 
 function Profiler ({id, phases, children}) {
-	if (!phases || phases.includes('mount')) console.log(`Profiler for ${id} init.`)
+	if (phases && phases.includes('mount')) console.log(`Profiler for ${id} init.`)
+	const queue = React.useRef([])
+	const sendData = (data) => {
+		client('profile', {data})
+	}
 
 	const callback = (...data) => {
 		const [id, phase, actualDuration, baseDuration, startTime, commitTime] = data
 		if (!phases || phases.includes(phase)) {
-			client('profile', {data: {
-				id, phase, actualDuration, baseDuration, startTime, commitTime
-			}})
+			const profilerData = {id, phase, actualDuration, baseDuration, startTime, commitTime}
+			queue.current.push(profilerData)
 		}
 	}
+
+	React.useEffect(() => {
+		const interval = setInterval(() => {
+			if (queue.current.length) {
+				const data = [...queue.current]
+				sendData(data)
+				queue.current = []
+			}
+		}, 5000)
+		return () => { clearInterval(interval)}
+	}, [])
+
 	return (
 		<React.Profiler id={id} onRender={callback}>
 			{children}
